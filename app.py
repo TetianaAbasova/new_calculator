a782de9f776b623184e580f1ec1806fdfec2bcd6
Татьяна <sweet95blood@gmail.com>
2024-12-13 17:09:22 +0200

    Add a simple calculator

---
+++ app.py
@@ -0,0 +1,10 @@
+number_1 = int(input("Enter a first number: "))
+comand = input("Enter a comand: ")
+number_2 = int(input("Enter a second number: "))
+
+if comand == "+":
+    print(number_1 + number_2)
+elif comand == "-":
+    print(number_1 - number_2)
+else:
+    print("Invalid command")
